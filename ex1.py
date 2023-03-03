ca1fedca2f98fe5f2953355790b9b70d977cbce1
Cloud User <centos@ip-10-0-0-91.ec2.internal>
2023-03-03 15:03:55 +0000

    te

--- ex1.py
+++ ex1.py
@@ -1,10 +1,20 @@
+"""
+This module is intended to check Jenkins server version
+"""
 import jenkinsapi
 from jenkinsapi.jenkins import Jenkins
 
 def get_server_instance():
-    jenkins_url = 'http://44.200.182.151:8080/'
-    server = Jenkins(jenkins_url, username='akhil', password='123')
+    """
+    This function is intended to get server instance
+    """
+    jenkins_url = 'http://18.207.240.57:8080/'
+    # The below line is to authenticate Jenkins server
+    server = Jenkins(jenkins_url, username='akhil', password='')
+    print("server", server)
     return server
 
 if __name__ == '__main__':
-    print (get_server_instance().version)
+    gf = get_server_instance()
+    jenkins_server_version = gf.version
+    print (jenkins_server_version)
